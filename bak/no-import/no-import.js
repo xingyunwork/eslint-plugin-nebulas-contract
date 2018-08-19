d28b518263c362a627b4d081bcd77a46d59778d6
yucopowo <yucopowo@gmail.com>
2018-08-19 22:18:49 +0800

    init

---
+++ bak/no-import/no-import.js
@@ -0,0 +1,39 @@
+/**
+ * @fileoverview Tests for no-proxy rule.
+ * @author yucopowo@gmail.com
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../rules/no-proxy");
+const RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+
+var o = {name: 1};var p = new Proxy(o,{get: function(target, prop, receiver){return target['name'];}});
+
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-proxy", rule, {
+    valid: [
+        "function Test(){}",
+    ],
+    invalid: [
+        {
+            code: "var o = {name: 1};\nvar p = new Proxy(o,{get: function(target, prop, receiver){return target['name'];}});",
+            errors: [{
+                ruleId: 'no-proxy',
+                message: "Unallowed use of `Proxy`",
+                type: "Identifier",
+            }],
+        }
+    ],
+});
