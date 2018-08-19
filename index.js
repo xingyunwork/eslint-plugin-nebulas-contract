a14e93234f0a09a75a3e2139cca6fa91a45fd3cf
yucopowo <yucopowo@gmail.com>
2018-08-19 23:28:26 +0800

    fix config

--- index.js
+++ index.js
@@ -8,21 +8,16 @@ module.exports = {
         "no-proxy": require('./rules/no-proxy'),
         "no-window": require('./rules/no-window'),
     },
-    rulesConfig: {
-        'param-names': 1,
-        'always-return': 1,
-        'no-return-wrap': 1,
-        'no-native': 0,
-        'catch-or-return': 1
-    },
     configs: {
         recommended: {
             env: {
-                node: true
+                "es6": true,
+                "node": true
             },
             globals: {
             },
             plugins: [
+                "nebulas-contract"
             ],
             rules: {
                 "available-libs": 'error',
